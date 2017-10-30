dfb97c19ccd5d8955a7f92a6581c116a3f377a4a
robmaz <31442300+robmaz@users.noreply.github.com>
2017-10-30 14:42:58 +0100

    Poolseq (#16)
    * Installer for poolSeq.
    
    * Added some indentations to poolseq.rb
    
    * * Removed existence check so that poolSeq will always be reinstalled (to
    allow updates).
    * Added caveat about updating dependencies.
    
    * Fixed archive location in poolseq.rb
    
    * Seems to work now, but requires R to be installed as a homebrew package.
    
    * update formula to new poolSeq version
    
    * Added Aiases entry

--- Formula/poolseq.rb
+++ Formula/poolseq.rb
@@ -1,8 +1,9 @@
 class Poolseq < Formula
   desc "Analyze and simulate Pool-Seq time series data"
   homepage "https://github.com/ThomasTaus/poolSeq/"
-  url "https://github.com/ThomasTaus/poolSeq/releases/download/v0.3.1/poolSeq_0.3.1.tar.gz"
-  sha256 "a38a1a0d79ec16ba6ec9227d3f61f037154146e1b882940ca013f8f98db43eb3"
+
+  url "https://github.com/ThomasTaus/poolSeq/releases/download/v0.3.2/poolSeq_0.3.2.tar.gz"
+  sha256 "42b90f7117b9d7de90f33303806b7bcf9a07ef53354e51a603cd889e6f68876b"
 
   bottle :unneeded
 
