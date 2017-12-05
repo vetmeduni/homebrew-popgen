0473cbc49a718ec09167a20b260b517847913c45
Rupert Mazzucco <Rupert.Mazzucco@gmail.com>
2017-12-05 15:59:02 +0100

    Working distmap formula (HEAD only for now)

---
+++ Formula/distmap.rb
@@ -0,0 +1,21 @@
+class Distmap < Formula
+  desc "Wrapper around various mappers for distributed computation with Hadoop"
+  homepage "https://sourceforge.net/projects/distmap/"
+  head "https://github.com/robmaz/distmap.git"
+
+  def install
+    bin.install Dir["bin/*"]
+    perl5lib = lib/"perl5/site_perl"
+    perl5lib.install Dir["lib/perl5/site_perl/*.pm"]
+    javalib = share/"java"
+    javalib.install "share/java/distmap"
+    libexec_linux = libexec/"linux"
+    libexec_linux.install Dir["libexec/linux/*"]
+    libexec_macos = libexec/"macos"
+    libexec_macos.install Dir["libexec/macos/*"]
+  end
+
+  test do
+    system "#{bin}/distmap", "--help"
+  end
+end
