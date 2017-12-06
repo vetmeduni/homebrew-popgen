ef6a96ff95e407af2d8dcf58ad4cbc0883c94f96
robmaz <31442300+robmaz@users.noreply.github.com>
2017-12-06 11:14:21 +0100

    Working distmap formula (HEAD only for now) (#19)
    * Working distmap formula (HEAD only for now)
    
    * Slightly more efficient way to do this
    since Homebrew keeps libexec/ private, we cannot
    pollute a pre-existing libexec/macos/ or libexec/linux/

---
+++ Formula/distmap.rb
@@ -0,0 +1,19 @@
+class Distmap < Formula
+  desc "Wrapper around various mappers for distributed computation with Hadoop"
+  homepage "https://sourceforge.net/projects/distmap/"
+  head "https://github.com/robmaz/distmap.git"
+
+  def install
+    bin.install Dir["bin/*"]
+    perl5lib = lib/"perl5/site_perl"
+    perl5lib.install Dir["lib/perl5/site_perl/*.pm"]
+    javalib = share/"java"
+    javalib.install "share/java/distmap"
+    libexec.install "libexec/linux"
+    libexec.install "libexec/macos"
+  end
+
+  test do
+    system "#{bin}/distmap", "--help"
+  end
+end
