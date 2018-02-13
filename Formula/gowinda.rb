56c71ad11e45676c6337f6a332bffdb21b4cd18e
Daniel Gómez-Sánchez <daniel.gomez.sanchez@hotmail.es>
2018-02-13 15:21:11 +0100

    Refactor Gowinda formula
    - Reduce number of characters from description (brew audit --strict
      failure)
    - Use libexec for jar file
    - Simplify inclusion of JAVA_OPTS
    - Refactor caveats
    - Refactor tests

--- Formula/gowinda.rb
+++ Formula/gowinda.rb
@@ -1,5 +1,5 @@
 class Gowinda < Formula
-  desc "Unbiased analysis of gene set enrichment for genome-wide association studies"
+  desc "Unbiased analysis of gene set enrichment for GWAs"
   homepage "https://sourceforge.net/projects/gowinda/"
   url "https://downloads.sourceforge.net/project/gowinda/Gowinda-1.12.jar"
   sha256 "bbbb462dbb28e04eff826fa97885c2ac962c4a111fe693e157b017cb2d1df0b2"
@@ -9,23 +9,18 @@ class Gowinda < Formula
   depends_on :java => ["1.8", :run]
 
   def install
-    java = share/"java"
-    java.install Dir["*.jar"]
-    bin.write_jar_script java/"Gowinda-1.12.jar", "gowinda"
-    inreplace "#{bin}/gowinda", "exec java ", "exec java ${JAVA_OPTS}"
+    libexec.install Dir["Gowinda-*.jar"][0]
+    bin.write_jar_script Dir[libexec/"Gowinda-*.jar"][0], "gowinda", "${JAVA_OPTS}"
   end
 
   def caveats
-    <<-EOS.undent
-      The Gowinda JAR file is installed to
-        #{HOMEBREW_PREFIX}/share/java
-      and is meant to be called via the "gowinda" launch script.
-      Pass java options via the environment variable JAVA_OPTS.
+    <<~EOS
+      To pass java options to the "gowinda" wrapper, use the environment variable JAVA_OPTS.
+        Example: JAVA_OPTS="-Xmx4g" gowinda
     EOS
   end
 
   test do
-    assert_match "USAGE", shell_output("java -jar #{share}/java/Gowinda-1.12.jar --version 2>&1", 1)
-    assert_match "USAGE", shell_output("#{bin}/gowinda --version 2>&1", 1)
+    assert_match "Gowinda", shell_output("#{bin}/gowinda --help 2>&1", 1)
   end
 end
