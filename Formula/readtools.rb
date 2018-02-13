cc12f6046df6b9ed4828e795c0b8667e14375bb0
Daniel Gómez-Sánchez <daniel.gomez.sanchez@hotmail.es>
2018-02-13 11:01:17 +0100

    ReadTools formula improvements
    - Use libexec instead of share/java
    - Simplify code for JAVA_OPTS (remove post_install)
    - Re-write caveats (and fix sintax)
    - Fix tests (brew test pass)
    - Fix issues from "brew audit --strict" (except alias, already included)

--- Formula/readtools.rb
+++ Formula/readtools.rb
@@ -9,26 +9,19 @@ class Readtools < Formula
   depends_on :java => "1.8+"
 
   def install
-    java = share/"java"
-    java.install Dir["*.jar"]
-    bin.write_jar_script java/"ReadTools.jar", "readtools"
-  end
-
-  def post_install
-    inreplace "#{bin}/readtools", "exec java ", "exec java ${JAVA_OPTS}"
+    libexec.install "ReadTools.jar"
+    bin.write_jar_script Dir[libexec/"ReadTools.jar"][0], "readtools", "${JAVA_OPTS}"
   end
 
   def caveats
-    <<-EOS.undent
-      The ReadTools JAR files are installed to
-        #{HOMEBREW_PREFIX}/share/java
-
-      Pass java options via the environment variable JAVA_OPTS
+    <<~EOS
+      To pass java options to the readtools wrapper, use the environment variable JAVA_OPTS.
+        Example: JAVA_OPTS="-Xmx4g" readtools
     EOS
   end
 
   test do
-    assert_match "USAGE", shell_output("java -jar #{share}/java/ReadTools.jar -h 2>&1", 1)
-    assert_match "USAGE", shell_output("#{bin}/readtools -h 2>&1", 1)
+    assert_match "USAGE", shell_output("java -jar #{libexec}/ReadTools.jar -h 2>&1", 0)
+    assert_match "USAGE", shell_output("#{bin}/readtools -h 2>&1", 0)
   end
 end
