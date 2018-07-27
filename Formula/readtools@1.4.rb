2d63c37e3b864b7ab1fd1c2e479a94f12e0b6264
Daniel Gómez-Sánchez <daniel.gomez.sanchez@hotmail.es>
2018-07-27 20:26:39 +0200

    Update ReadTools formula to 1.5.0

--- Formula/readtools@1.4.rb
+++ Formula/readtools@1.4.rb
@@ -1,8 +1,8 @@
 class Readtools < Formula
   desc "Handling Sequence Data from Different Sequencing Platforms"
   homepage "https://magicdgs.github.io/ReadTools/"
-  url "https://github.com/magicDGS/ReadTools/releases/download/1.5.0/ReadTools.jar"
-  sha256 "7f77e477c24784e97b7e8a931a6d5718c24fdaa10d18c3bf6f4012f170d9874d"
+  url "https://github.com/magicDGS/ReadTools/releases/download/1.4.1/ReadTools.jar"
+  sha256 "7092f3de1de6eb24023b1a73a0e362feebc98145ad07310ef5c0293fe2934635"
 
   head { url "https://github.com/magicDGS/ReadTools.git" }
 
