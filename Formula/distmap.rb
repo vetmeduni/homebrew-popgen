660e8565e981adf2b6f142a78010a6fc52af9db6
Rupert Mazzucco <Rupert.Mazzucco@gmail.com>
2019-01-25 14:35:54 +0100

    Put head back into distmap.rb

--- Formula/distmap.rb
+++ Formula/distmap.rb
@@ -3,6 +3,7 @@ class Distmap < Formula
   homepage "https://sourceforge.net/projects/distmap/"
   url "https://github.com/robmaz/distmap/archive/v.3.1.0.tar.gz"
   sha256 "eb777705825f615712c587649a365e58d19b09dc589552bee34eb8ae6d49b20e"
+  head "https://github.com/robmaz/distmap.git"
 
   def install
     bin.install Dir["bin/*"]
