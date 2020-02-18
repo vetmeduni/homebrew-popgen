ee0e286ed6ab2a2934fe7f2487c97fc324cd16b9
Rupert Mazzucco <Rupert.Mazzucco@gmail.com>
2020-02-18 18:27:48 +0100

    Make popte2.rb pass brew audit

--- Formula/popte2.rb
+++ Formula/popte2.rb
@@ -1,8 +1,5 @@
 class Popte2 < Formula
-  desc "Enables comparative population genomics of transposable elements (TE). As a major innovation
-  PoPoolation TE2 introduces the physical pileup file which allows to homogenize the power to identify
-  TEs and thus enables an unbiased comparison of TE abundance between samples, where samples could be
-  pooled populations, tissues or sequenced individuals."
+  desc "Comparative population genomics of transposable elements"
   homepage "https://sourceforge.net/projects/popoolation-te2/"
   url "https://downloads.sourceforge.net/project/popoolation-te2/previous-releases/popte2-v1.10.04.jar"
   sha256 "58798c37ed3ff7d5f24d44af3108af485298fbea0eba1a27e535af2e6ff1bb5a"
