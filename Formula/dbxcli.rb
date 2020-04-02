807c0d881a2a65714a8edc87aeb453d1623d5f88
Rupert Mazzucco <Rupert.Mazzucco@gmail.com>
2020-04-02 12:37:06 +0200

    remove some comment lines

--- Formula/dbxcli.rb
+++ Formula/dbxcli.rb
@@ -1,6 +1,3 @@
-# Documentation: https://docs.brew.sh/Formula-Cookbook
-#                https://rubydoc.brew.sh/Formula
-# PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
 class Dbxcli < Formula
   desc "A command line client for Dropbox"
   homepage "http://dropbox.github.io/dbxcli/"
